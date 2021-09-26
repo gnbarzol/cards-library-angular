28457e7a5fd275cbef06f3f505a150727f902e13
gnbarzol <gnbarzol@espol.edu.ec>
2021-09-25 21:29:43 -0500

    Create library

---
+++ projects/cards/src/lib/cards.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gb-cards',
+  template: `
+    <p>
+      cards works test!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/cards/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of cards (Lo que queremos que se publique al exterior)
+ */
+
+export * from './lib/cards.service';
+export * from './lib/cards.component';
+export * from './lib/cards.module';
